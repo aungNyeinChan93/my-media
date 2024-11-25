6863ece58e6750ccc16cde6b04fff6d926b9f996
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-11-25 17:03:25 +0630

    trend post list added

--- app/Http/Controllers/TrendPostController.php
+++ app/Http/Controllers/TrendPostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ActionLog;
 use Illuminate\Http\Request;
 
 class TrendPostController extends Controller
@@ -9,6 +10,12 @@ class TrendPostController extends Controller
     // trend posts index page
     public function index()
     {
-        return view("admin.trendPost.index");
+        // $actionLogs = ActionLog::query()->groupBy('post_id')->count();
+
+        $posts = ActionLog::select('action_logs.*', 'posts.*')
+            ->leftJoin('posts', 'posts.id', '=', 'action_logs.post_id')
+            ->paginate(5);
+        // dd($posts);
+        return view("admin.trendPost.index", compact('posts'));
     }
 }
--- resources/views/admin/trendPost/index.blade.php
+++ resources/views/admin/trendPost/index.blade.php
@@ -2,7 +2,54 @@
 
 @section('content')
     <div class="container trendPost">
-        <h3 class="bg-primary p-1 text-center rounded ">Trend Posts</h3>
+        <div class="row">
+            <div class="col-12">
+                <div class="card my-3">
+                    <div class="card-header">
+                        <h3 class="text-secondary p-1">Trend Posts</h3>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered table-hover p-2">
+                            <thead class="bg-secondary">
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Post Name</th>
+                                    <th>View Counts</th>
+                                    <th>Post Image</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($posts as $post)
+                                    <tr>
+                                        <td>{{ $post->id }}</td>
+                                        <td>{{ $post->title }}</td>
+                                        <td>
+                                            view Counts
+                                        </td>
+                                        <td>
+                                            @if ($post->image == null)
+                                                <img src="{{ asset('/postsImage/default.png') }}" alt="img"
+                                                    class=" img-fluid w-25">
+                                            @else
+                                                <img src="{{ asset('/postsImage/' . $post->image) }}" alt=""
+                                                    class=" img-fluid w-25">
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <button class="btn btn-secondary">Detail</button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="paginate mt-4">
+                            {{ $posts->links('pagination::bootstrap-5') }}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 @endsection
